7fb21d08812234c02b683b5ded2ad462d3fe8082
Paolo Furini <paolo.furini@gmail.com>
2018-03-13 13:16:57 +0100

    Update with new code to determine if the user has been authenticated externally

--- modules/web/src/it/nexbit/cuba/security/userprofile/web/companions/UserEditProfileCompanion.java
+++ modules/web/src/it/nexbit/cuba/security/userprofile/web/companions/UserEditProfileCompanion.java
@@ -9,6 +9,7 @@ import com.haulmont.cuba.core.sys.AppContext;
 import com.haulmont.cuba.core.sys.SecurityContext;
 import com.haulmont.cuba.security.global.UserSession;
 import com.haulmont.cuba.web.App;
+import com.haulmont.cuba.web.security.ExternalUserCredentials;
 import com.haulmont.cuba.web.sys.WebUserSessionSource;
 import com.vaadin.server.VaadinSession;
 import it.nexbit.cuba.security.userprofile.UserEditProfile;
@@ -18,14 +19,12 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
-import static com.haulmont.cuba.web.auth.ExternallyAuthenticatedConnection.EXTERNAL_AUTH_USER_SESSION_ATTRIBUTE;
-
 public class UserEditProfileCompanion implements UserEditProfile.Companion {
 
     @Override
     public Boolean isLoggedInWithExternalAuth() {
         UserSession userSession = AppBeans.get(UserSession.class);
-        return Boolean.TRUE.equals(userSession.getAttribute(EXTERNAL_AUTH_USER_SESSION_ATTRIBUTE));
+        return ExternalUserCredentials.isLoggedInWithExternalAuth(userSession);
     }
 
     @Override
